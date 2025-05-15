3e3c6bc9e7899789523ab78d8483644dc021bbfe
Zeyu Zhou <zeyuzhou91@gmail.com>
2025-05-15 17:01:02 -0400

    commented out classes and functions in aux.py

--- src/mira_km/utils/aux.py
+++ src/mira_km/utils/aux.py
@@ -11,205 +11,210 @@ from numpy.typing import NDArray
 # from ..core import ROI, Environment
 
 
-
-class Environment:
-    def __init__(self):
+# To remove Environment
+# class Environment:
+#     def __init__(self):
         
-        self.root_dir = ''  # root directory path
-        self.subj_dir = ''  # directory path of the subject
-        self.MRI_dir = ''   # directory path for MRI images and segmentations
-        self.PET_dir = ''   # directory path for PET images
-        self.AIF_dir = ''   # directory path for AIF data
-        self.km_dir = ''    # directory path for kinetic modeling (temp)
-        self.masks_dir = '' # directory path for masks 
-        self.seg_path = ''  # path of the segmentation file
-        self.mr2pet_lta_path = '' # path of the MR to PET linear transformation file
-        self.pet2mr_lta_path = '' # path of the PET to MR linear transformation file
-        self.framedurationfile_path = '' # path of the frame durations file
-        self.framemidpointfile_path = '' # path of the frame mid-points file 
-        self.AIF_ptac_path = ''
-        self.AIF_pif_path = ''
-        self.AIF_p2wb_ratio_path = ''
+#         self.root_dir = ''  # root directory path
+#         self.subj_dir = ''  # directory path of the subject
+#         self.MRI_dir = ''   # directory path for MRI images and segmentations
+#         self.PET_dir = ''   # directory path for PET images
+#         self.AIF_dir = ''   # directory path for AIF data
+#         self.km_dir = ''    # directory path for kinetic modeling (temp)
+#         self.masks_dir = '' # directory path for masks 
+#         self.seg_path = ''  # path of the segmentation file
+#         self.mr2pet_lta_path = '' # path of the MR to PET linear transformation file
+#         self.pet2mr_lta_path = '' # path of the PET to MR linear transformation file
+#         self.framedurationfile_path = '' # path of the frame durations file
+#         self.framemidpointfile_path = '' # path of the frame mid-points file 
+#         self.AIF_ptac_path = ''
+#         self.AIF_pif_path = ''
+#         self.AIF_p2wb_ratio_path = ''
 
 
-class ROI:
-    def __init__(self, name):
-        self.name = name     # string
-        self.ID = None       # integer
-        self.num_frames = None   # positive integer, 1 for single frame
-        self.num_voxels = None  # integer
-        self.vol_ml = None   # volume in [mL]
+# To remove ROI
+# class ROI:
+#     def __init__(self, name):
+#         self.name = name     # string
+#         self.ID = None       # integer
+#         self.num_frames = None   # positive integer, 1 for single frame
+#         self.num_voxels = None  # integer
+#         self.vol_ml = None   # volume in [mL]
         
-        self.avg_intensity = []  # list of decimals, average intensity of the voxels (mean of voxels) 
-        self.tot_intensity = []  # list of decimals, total intensity of the voxels (sum of voxels)
-        self.concentration = []     # list of decimals, average concentration in [Bq/mL], for dynamic imaging          
+#         self.avg_intensity = []  # list of decimals, average intensity of the voxels (mean of voxels) 
+#         self.tot_intensity = []  # list of decimals, total intensity of the voxels (sum of voxels)
+#         self.concentration = []     # list of decimals, average concentration in [Bq/mL], for dynamic imaging          
 
-        self.onetcm_params = {'K1': None, 'k2': None, 'VD': None}  
-        self.twotcm_params = {'K1': None, 'k2': None, 'k3': None, 'k4':None, 'VND':None, 'VT':None, 'VS':None, 'BPND':None}              
+#         self.onetcm_params = {'K1': None, 'k2': None, 'VD': None}  
+#         self.twotcm_params = {'K1': None, 'k2': None, 'k3': None, 'k4':None, 'VND':None, 'VT':None, 'VS':None, 'BPND':None}              
 
 
-class FrameSchedule:
-    def __init__(self, durations=None, mid_points=None):
-        self.durations = []   # list of numbers
-        self.start_points = []       # list of numbers
-        self.mid_points = []         # list of numbers
+# To remove FrameSchedule
+# class FrameSchedule:
+#     def __init__(self, durations=None, mid_points=None):
+#         self.durations = []   # list of numbers
+#         self.start_points = []       # list of numbers
+#         self.mid_points = []         # list of numbers
         
-        if mid_points == None:
-            self.calculate_attributes_from_durations(durations)
-        elif durations == None:
-            self.calculate_attributes_from_midpoints(mid_points)
+#         if mid_points == None:
+#             self.calculate_attributes_from_durations(durations)
+#         elif durations == None:
+#             self.calculate_attributes_from_midpoints(mid_points)
         
-    def calculate_attributes_from_durations(self, durations):
-        self.durations = durations
+#     def calculate_attributes_from_durations(self, durations):
+#         self.durations = durations
         
-        cur_start = 0.0
-        for duration in self.durations:
+#         cur_start = 0.0
+#         for duration in self.durations:
             
-            self.start_points.append(cur_start)
+#             self.start_points.append(cur_start)
             
-            nxt_start = cur_start + duration
+#             nxt_start = cur_start + duration
             
-            mid = (cur_start + nxt_start) / 2.0
-            self.mid_points.append(mid)
+#             mid = (cur_start + nxt_start) / 2.0
+#             self.mid_points.append(mid)
             
-            cur_start = nxt_start
+#             cur_start = nxt_start
     
-        return None
+#         return None
 
-    def calculate_attributes_from_midpoints(self, mid_points):
-        self.mid_points = mid_points
+#     def calculate_attributes_from_midpoints(self, mid_points):
+#         self.mid_points = mid_points
         
-        cur_start = 0.0
-        for mid in self.mid_points:
+#         cur_start = 0.0
+#         for mid in self.mid_points:
             
-            self.start_points.append(cur_start)
+#             self.start_points.append(cur_start)
             
-            duration = 2 * (mid - cur_start)
-            self.durations.append(duration)
+#             duration = 2 * (mid - cur_start)
+#             self.durations.append(duration)
             
-            nxt_start = cur_start + duration
+#             nxt_start = cur_start + duration
             
-            cur_start = nxt_start
+#             cur_start = nxt_start
     
-        return None
+#         return None
             
 
+
+# To remove TimeCurve
 # Timed curve of a quantity
-class TimeCurve:
-    def __init__(self, name):
-        self.name = name   # string, name of the quantity, or name of the tissue 
-        self.t_data = []     # list of floats, time points
-        self.y_data = []   # list of floats, values of quantity
-        self.t_unit = ''     # string
-        self.y_unit = ''  # string, unit of measured quantity
-        self.fitfunc = None  # function, the fitting function
-        self.fitparams = None   # numpy.ndarray, parameters of the fitting function
+# class TimeCurve:
+#     def __init__(self, name):
+#         self.name = name   # string, name of the quantity, or name of the tissue 
+#         self.t_data = []     # list of floats, time points
+#         self.y_data = []   # list of floats, values of quantity
+#         self.t_unit = ''     # string
+#         self.y_unit = ''  # string, unit of measured quantity
+#         self.fitfunc = None  # function, the fitting function
+#         self.fitparams = None   # numpy.ndarray, parameters of the fitting function
 
-    def plot(self, xlim=None, ylim=None):
-        """
-        Plot the data and fitted curve. 
-        """
+#     def plot(self, xlim=None, ylim=None):
+#         """
+#         Plot the data and fitted curve. 
+#         """
         
-        plt.figure()
-        if self.t_data != [] and self.y_data != []: 
-            plt.scatter(self.t_data, self.y_data, c='blue', label='data')
-        if self.t_data != [] and self.fitfunc != None and self.fitparams.all() != None:
-            tmax = np.max(self.t_data)
-            tfit = np.linspace(0, tmax*1.1, 1000)
-            yfit = self.fitfunc(tfit, *self.fitparams)
-            plt.plot(tfit, yfit, c='red', label='fit')
-        plt.xlabel(f'Time ({self.t_unit})')
-        if self.y_unit == 'unitless':
-            plt.ylabel(f'{self.name}')
-        else:
-            plt.ylabel(f'{self.name} ({self.y_unit})')
-        if xlim == None:
-            pass
-        else:
-            plt.xlim(xlim)
-        if ylim == None:
-            pass
-        else:
-            plt.ylim(ylim)
-        plt.legend()
-        plt.show()
+#         plt.figure()
+#         if self.t_data != [] and self.y_data != []: 
+#             plt.scatter(self.t_data, self.y_data, c='blue', label='data')
+#         if self.t_data != [] and self.fitfunc != None and self.fitparams.all() != None:
+#             tmax = np.max(self.t_data)
+#             tfit = np.linspace(0, tmax*1.1, 1000)
+#             yfit = self.fitfunc(tfit, *self.fitparams)
+#             plt.plot(tfit, yfit, c='red', label='fit')
+#         plt.xlabel(f'Time ({self.t_unit})')
+#         if self.y_unit == 'unitless':
+#             plt.ylabel(f'{self.name}')
+#         else:
+#             plt.ylabel(f'{self.name} ({self.y_unit})')
+#         if xlim == None:
+#             pass
+#         else:
+#             plt.xlim(xlim)
+#         if ylim == None:
+#             pass
+#         else:
+#             plt.ylim(ylim)
+#         plt.legend()
+#         plt.show()
         
-        return None
+#         return None
     
-    def print_fitparams(self):
-        """
-        Print the fitting parameters. 
-        """
+#     def print_fitparams(self):
+#         """
+#         Print the fitting parameters. 
+#         """
         
-        print('Fitting parameters:\n')
-        print(self.fitparams)
-        return None
+#         print('Fitting parameters:\n')
+#         print(self.fitparams)
+#         return None
         
 
-def plot_timecurve(tc):
+# To remove plot_timecurve
+# def plot_timecurve(tc):
     
         
-    tmax = np.max(tc.t_data)
-    tfit = np.linspace(0, tmax*1.1, 1000)
-    yfit = tc.fitfunc(tfit, *tc.fitparams)
+#     tmax = np.max(tc.t_data)
+#     tfit = np.linspace(0, tmax*1.1, 1000)
+#     yfit = tc.fitfunc(tfit, *tc.fitparams)
     
-    plt.figure()
-    plt.scatter(tc.t_data, tc.y_data, c='blue', label='data')
-    plt.plot(tfit, yfit, c='red', label='fit')
-    plt.xlabel(f'Time ({tc.t_unit})')
-    plt.ylabel(f'{tc.name} ({tc.y_unit})')
-    plt.legend()
-    plt.show()
+#     plt.figure()
+#     plt.scatter(tc.t_data, tc.y_data, c='blue', label='data')
+#     plt.plot(tfit, yfit, c='red', label='fit')
+#     plt.xlabel(f'Time ({tc.t_unit})')
+#     plt.ylabel(f'{tc.name} ({tc.y_unit})')
+#     plt.legend()
+#     plt.show()
     
-    return None
+#     return None
     
 
-
-def delete_folder_contents(folder):
+# To remove delete_folder_contents (implenmented in file_handling)
+# def delete_folder_contents(folder):
     
-    for filename in os.listdir(folder):
-        file_path = os.path.join(folder, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception as e:
-            print('Failed to delete %s. Reason: %s' % (file_path, e))
+#     for filename in os.listdir(folder):
+#         file_path = os.path.join(folder, filename)
+#         try:
+#             if os.path.isfile(file_path) or os.path.islink(file_path):
+#                 os.unlink(file_path)
+#             elif os.path.isdir(file_path):
+#                 shutil.rmtree(file_path)
+#         except Exception as e:
+#             print('Failed to delete %s. Reason: %s' % (file_path, e))
         
-    return None
+#     return None
 
 
-
-def extract_file_name(file_path):
-    """
-    Extract the file base name and extension from its path. 
+# To move extract_file_name to file.py
+# def extract_file_name(file_path):
+#     """
+#     Extract the file base name and extension from its path. 
     
-    Example: /home/documents/mask_cerebellum.nii.gz
-    Return: mask_cerebellum and .nii.gz
+#     Example: /home/documents/mask_cerebellum.nii.gz
+#     Return: mask_cerebellum and .nii.gz
 
-    Parameters
-    ----------
-    file_path : string, file path
-        The full path of the file.
+#     Parameters
+#     ----------
+#     file_path : string, file path
+#         The full path of the file.
 
-    Returns
-    -------
-    basename : string
-        The base name of the file. 
-    extension: string
-        The extension of the file. 
-    """
+#     Returns
+#     -------
+#     basename : string
+#         The base name of the file. 
+#     extension: string
+#         The extension of the file. 
+#     """
 
-    # os.path.basename returns the full name of the file, including the extension
-    fullname = os.path.basename(file_path)
+#     # os.path.basename returns the full name of the file, including the extension
+#     fullname = os.path.basename(file_path)
     
-    # split fullname from the first occurrence of '.'
-    # e.g.: mask_cerebellum.nii.gz => mask_cerebellum and nii.gz
-    basename, extension = fullname.split('.', 1)
-    extension = '.' + extension
+#     # split fullname from the first occurrence of '.'
+#     # e.g.: mask_cerebellum.nii.gz => mask_cerebellum and nii.gz
+#     basename, extension = fullname.split('.', 1)
+#     extension = '.' + extension
     
-    return basename, extension
+#     return basename, extension
     
 
 
@@ -542,45 +547,11 @@ def model_unit_table(model_name: str) -> dict[str, str]:
 
 
             
-if __name__ == "__main__":
+# if __name__ == "__main__":
     
-    # test
+
     
-    # file_path = "/home/documents/mask_cerebellum.nii.gz"
-    
-    # base, extension = extract_file_name(file_path)
-    # print(base)
-    # print(extension)
-    
-    # env = Environment()
-    # env.MRI_dir = '/zeyu/documents/Data/FEPPA/F301/Freesurfer/bert1/mri'
-    # env.PET_dir = '/zeyu/documents/Data/FEPPA/F301/PET'
-    # print(env.MRI_dir)
-    # print(env.PET_dir)
-    
-    # header = 'concentration'
-    # arr = [10, 20, 30, 40, 50]
-    # write_to_csv_onecol(header, arr, '/Users/zeyuzhou/Documents/kinetic_modeling_test/FEPPA_20190523_AA_31002478/test.csv')
-    
-    
-    # dur = [1, 2, 5, 6, 10]
-    # FS = FrameSchedule(dur)
-    # print(FS.durations)
-    # print(FS.start_points)
-    # print(FS.mid_points)
-    
-    # col1_data, col2_data = read_from_csv_twocols('/Users/zeyuzhou/Documents/kinetic_modeling_test/FEPPA_20190523_AA_31002478/AIF/arterial_plasma_tac.csv')
-    # print(col1_data)
-    # print(col2_data)
-    
-    
-    f = np.array([1,1,2,1])
-    t = np.array([1,2,4,8])
-    intf = discrete_integrate(f, t)
-    
-    print('intf =', intf)
-    
-    pass
+#     pass
     
     
     
